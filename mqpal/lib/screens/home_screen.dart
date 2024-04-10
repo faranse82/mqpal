73e31c6bdd8f137d5b64346529396d47b8d260db
Faran Sepehrisadr <faran.sepehrisadr1@students.mq.edu.au>
2024-04-11 09:38:20 +1000

    started working on home_screen

---
+++ mqpal/lib/screens/home_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(context) {}
+}
